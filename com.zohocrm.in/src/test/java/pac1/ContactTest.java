85789e7964cc102d1a6835d8dcada16be987f68a
Sack <Sack@DESKTOP-GVKU7GV>
2025-03-13 17:18:18 +0530

    added project , please check

---
+++ com.zohocrm.in/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,14 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createContacttest() {
+	System.out.println("createContact");
+}
+@Test
+public void modifyContacttest() {
+	System.out.println("modifyContact");
+}
+}
---
+++ com.zohocrm.in/src/test/java/pac2/orgTest.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+@Test
+public void createOrgtest() {
+	System.out.println("createContact");
+}
+@Test
+public void modifyOrgtest() {
+	System.out.println("modifyContact");
+}
+}
