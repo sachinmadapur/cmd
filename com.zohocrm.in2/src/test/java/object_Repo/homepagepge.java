140afe27a8d16793ee8c7dd72ae9fbf57cfba767
Nishan-M <112767849+Nishan-M@users.noreply.github.com>
2025-03-14 15:54:00 +0530

    Update homepagepge.java

--- com.zohocrm.in2/src/test/java/object_Repo/homepagepge.java
+++ com.zohocrm.in2/src/test/java/object_Repo/homepagepge.java
@@ -3,6 +3,7 @@ package object_Repo;
 public class homepagepge {
 public static void main(String[] args) {
 	System.out.println("Hi nishant Holi Party ready in 4:30 ");
-	System.out.println("Hi Sachin Holi Party ready in 4:30");
+	System.out.println("Hi Sachin Holi ");
+	System.out.println("Happy Holi everyone");
 }
 }
