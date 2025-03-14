b3f9e5497a769f3f5dca357342c219f46667b6ad
Imteyaz <imteyaza285@gmail.com>
2025-03-14 15:48:28 +0530

    Merge branch 'master' of https://github.com/sachinmadapur/cmd.git

--- com.zohocrm.in2/src/test/java/object_Repo/homepagepge.java
+++ com.zohocrm.in2/src/test/java/object_Repo/homepagepge.java
@@ -2,6 +2,7 @@ package object_Repo;
 
 public class homepagepge {
 public static void main(String[] args) {
-	System.out.println("hey nishant");
+	System.out.println("Hi nishant Holi Party ready in 4:30 ");
+	System.out.println("Hi Sachin Holi Party ready in 4:30");
 }
 }
